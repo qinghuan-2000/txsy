7f425b2e8fdfdefa28128a919ee6155d47e8bebe
gsxt <1290777504@qq.com>
2021-06-24 17:15:17 +0800

    06-24 enter

--- miniprogram/pages/test/test.js
+++ miniprogram/pages/test/test.js
@@ -1,66 +1,18 @@
-// pages/test/test.js
+//获取应用实例
+const app = getApp()
+ 
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    //顶部安全距离（状态栏高度）
+    statusBarHeight: wx.getSystemInfoSync().statusBarHeight,
+    list: [
+      {id: 1,date:'2020-07-01',timeStatr:'08:10',timeEnd:'10:10',countNum:15,num:2,statusName:'已完成'},
+      {id: 2,date:'2020-07-02',timeStatr:'08:10',timeEnd:'10:10',countNum:15,num:8,statusName:'已完成'},
+      {id: 3,date:'2020-07-03',timeStatr:'08:10',timeEnd:'10:10',countNum:15,num:3,statusName:'已完成'},
+      {id: 4,date:'2020-07-04',timeStatr:'08:10',timeEnd:'10:10',countNum:15,num:2,statusName:'已完成'},
+      {id: 5,date:'2020-07-06',timeStatr:'08:10',timeEnd:'10:10',countNum:15,num:6,statusName:'已完成'},
+      {id: 6,date:'2020-07-08',timeStatr:'08:10',timeEnd:'10:10',countNum:15,num:2,statusName:'已完成'},{id: 7,date:'2020-07-09',timeStatr:'08:10',timeEnd:'10:10',countNum:15,num:2,statusName:'已完成'}
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+ 
 })
