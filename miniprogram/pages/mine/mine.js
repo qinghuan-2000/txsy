1cbb709fa6956e2fa3dcbab2fa73f6fd946e3679
gsxt <1290777504@qq.com>
2021-06-25 13:56:19 +0800

    06-25 mine

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -1,18 +1,45 @@
 // pages/mine/mine.js
+
+var app = getApp
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    root: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    // 查看是否授权
+    wx.getSetting({
+      success (res){
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function(res) {
+              console.log(res.userInfo)
+              that.setData({
+                result:'ok',// 结果
+                nickName:res.userInfo.nickName,// 微信昵称
+                avatarUrl:res.userInfo.avatarUrl,// 微信头像
+              })
+            }
+          })
+        }else{
+          // 未授权，结果返回null
+          that.setData({
+            result:'null',// 结果
+          })
+        }
+      }
+    })
   },
 
   /**
@@ -62,5 +89,24 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  bindGetUserInfo (e) {
+    console.log(e.detail.userInfo.nickName)
+    var that = this;
+    that.setData({
+      result:'ok',// 结果
+      nickName:e.detail.userInfo.nickName,// 微信昵称
+      avatarUrl:e.detail.userInfo.avatarUrl,// 微信头像
+    })
+  },
+
+  popevent:function(){
+    wx.showModal({
+      title: '提示',
+      content: '请先授权登录',
+      showCancel: false,
+
+    })
   }
 })
